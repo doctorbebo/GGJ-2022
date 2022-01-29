cb6f7959f960f4f1638c4ea65c2189d4a4b27a60
Byron Brown <byronrb92@gmail.com>
2022-01-28 22:35:09 -0500

    Add sprite Asteroids

--- Assets/_Scripts/Byron_scripts/ObstacleMover.cs
+++ Assets/_Scripts/Byron_scripts/ObstacleMover.cs
@@ -1,22 +1,28 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class ObstacleMover : MonoBehaviour
 {
-    private Rigidbody rb;
+    private Rigidbody2D rb;
     public float thrust = 30;
+    public float rotationSpeed = 5;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void OnEnable()
     {
         rb.AddForce(Vector3.left * thrust * 10);
+
+        if (Random.Range(0, 2) == 0)
+            rotationSpeed *= -1;
+
+        rb.AddTorque(rotationSpeed);
+        transform.Rotate(0, 0, Random.Range(0, 360));
     }
 
     private void OnDisable()
