942a353c1632decbb0dec4c13208007bf667c677
Ehab Hamed <66770268+Ehab-A-Hamed@users.noreply.github.com>
2022-01-29 13:41:02 -0500

    Player1

---
+++ Assets/_Scripts/Gun.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] float destroyTime = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject gameObject = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            Destroy(gameObject, destroyTime);
+        }
+    }
+}
