7889e4241d80debcc139eda8c91c46c4fafefe5a
Ehab Hamed <66770268+Ehab-A-Hamed@users.noreply.github.com>
2022-01-30 11:35:49 -0500

    MainMenu

--- Assets/ButtonEffect.cs
+++ Assets/ButtonEffect.cs
@@ -7,7 +7,7 @@ public class ButtonEffect : MonoBehaviour
 
     public void ZoomIn()
     {
-        gameObject.transform.localScale = new Vector2(1.1f, 1.1f);
+        gameObject.transform.localScale = new Vector2(1.02f, 1.02f);
     }
 
     public void ZoomOut()
