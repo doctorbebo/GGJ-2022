f4b4bd3cf9af3a0d62e1d92ca4901abe63778ce8
Andrew Zoerb <azoerb1@gmail.com>
2022-01-29 19:33:15 -0500

    Add damage scripts to ship, D to damage for testing purposes

---
+++ Assets/_Scripts/Damagable.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Damagable : MonoBehaviour {
+  private GameObject damageSpritePrefab;
+  public Sprite damageSprite;
+  Color[] colors = { Color.cyan, Color.yellow, Color.magenta };
+  int colorIndex = 0;
+
+  [SerializeField] float damageDistance = 0.03f;
+  [SerializeField] int numDamageSprites = 7;
+
+  void Awake() {
+    damageSpritePrefab = Resources.Load("Damage Sprite Prefab") as GameObject;
+  }
+
+  void Update() {
+    if (Input.GetKeyDown(KeyCode.D)) {
+      Damage();
+    }
+  }
+
+  void Damage() {
+    damageSpritePrefab.GetComponent<SpriteRenderer>().sprite = damageSprite;
+    for (int i = 0; i < numDamageSprites; i++) {
+      GameObject go = Instantiate(damageSpritePrefab, transform.position + Vector3.forward, transform.rotation);
+      go.transform.parent = transform;
+      Vector2 velocity = Quaternion.Euler(0f, 0f, 360 / numDamageSprites * i) * Vector2.up * damageDistance;
+      go.GetComponent<DamageSprite>().Init(colors[colorIndex++ % colors.Length], velocity);
+    }
+  }
+}
